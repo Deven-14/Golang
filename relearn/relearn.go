1495b85feb02db0c975ca6c6a91c1a0c78c66010
Deven-14 <deven.paramaj@gmail.com>
2025-03-29 18:25:47 +0530

    Add examples of flow control statements

--- relearn/relearn.go
+++ relearn/relearn.go
@@ -19,9 +19,43 @@ const (
 	Small = Big >> 99
 )
 
+func sumOfN1(n int) (sum int) {
+	for i := 0; i < n; i++ {
+		sum += i
+	}
+	sum = 0
+	// new way
+	for j := range n {
+		sum += j
+	}
+	return sum
+}
+
+func sumOfN2(n int) (sum int) {
+	i := 0
+	for i < n { // for ; i < n;
+		sum += i
+		i++
+	}
+	return sum
+}
+
 func main() {
 	var a, b = 1, true
 	var x, y = swap(3, 4)
 	c := float64(a)
 	fmt.Println("abc", rand.Intn(10), add(3, 4), a, b, x, y, c, float64(Big), Small)
+
+	fmt.Println(sumOfN1(5), sumOfN2(5))
+
+	if x == 4 {
+		fmt.Println(a, b)
+	}
+
+	if z := 5; z > 0 {
+		fmt.Println(z)
+	} else {
+		fmt.Println(-z)
+	}
+
 }
